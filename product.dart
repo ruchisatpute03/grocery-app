15d7e4ce7ae40f3466f84ec49591a3fcd6be8ea2
Ruchi Satpute <satputeruchi82@gmail.com>
2025-08-02 17:39:08 +0530

    Add files via upload

---
+++ product.dart
@@ -0,0 +1,21 @@
+class Product {
+  final String id;
+  final String name;
+  final String imageUrl;
+  final double price;
+  final double discount;
+  final double rating;
+  final String deliveryTime;
+  final String description;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+    required this.price,
+    required this.discount,
+    required this.rating,
+    required this.deliveryTime,
+    required this.description,
+  });
+}
